6aa62c2320088069953bbb438a4691ef94c1b1d4
PratyansuPrateek <pratyansuprateek16@gmail.com>
2026-06-03 09:16:22 +0000

    Variable basics

---
+++ 01_Basics/01_Variable.js
@@ -0,0 +1,17 @@
+const accId=123
+let accEmail="pratyansu@Gmail.com"
+var accPassword="1234"
+accCity="Bengaluru"
+let accState;
+
+// prefore not to use var beacuse issue in block scoop and function scoop
+
+// we can define variable with out using key words like var, const, let but it is not prefored
+
+//if a declear a variable and did not assign the value then the by default value is "Undefine".
+
+/*console.table([accId,accEmail,accPassword,accCity,accState])
+    this means instead of doing consol.log multiple time we can print the value 
+    in single time by table format 
+*/
+console.table([accId,accEmail,accPassword,accCity,accState])
