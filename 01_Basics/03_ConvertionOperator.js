0b3b53098888282ce49a5cbac53e7b2f92515d82
PratyansuPrateek <pratyansuprateek16@gmail.com>
2026-06-08 17:38:06 +0000

    Convertion of Datatype

---
+++ 01_Basics/03_ConvertionOperator.js
@@ -0,0 +1,31 @@
+let score = "happy"
+let scoreNumber = Number(score)
+console.log(scoreNumber);
+console.log(typeof scoreNumber);
+
+/*
+    If we want to conver one data type to another like any other data type to number just we need to do Number() 
+    like this only we can conver any thing to anything like String(), Boolean(), etc.
+
+    if we want to conver null, empty string value then value became 0 like other to conver undefine/String/number + String it became 
+    NaN(Not a Number) but the type of it is Number only 
+*/
+
+let number = -1245
+let numberBoolean = Boolean(number)
+
+// console.log(numberBoolean);
+// console.log(typeof numberBoolean);
+
+
+let name = "Happy"
+let nameBoolean = Boolean(name)
+// console.log(nameBoolean);
+// console.log(typeof nameBoolean);
+
+/*
+    here we want to conver the number like 0 and 1 or bigger then one it will false and true
+    like to conver string to boolean then if empty string then false and for normal string it will be true
+*/
+
+
